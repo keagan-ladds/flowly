25687c51f74c9d49d45e56a708cc832cbae26b7d
Keagan Ladds <ladds.k@gmail.com>
2023-10-23 12:39:24 +0200

    Test PR change

--- src/libraries/Flowly.Core/src/Logging/ILogger.cs
+++ src/libraries/Flowly.Core/src/Logging/ILogger.cs
@@ -6,7 +6,7 @@ namespace Flowly.Core.Logging
     {
         void Debug(string message);
         void Info(string message);
-        void Warn(string message);
+        void Warn(string message); 
         void Error(string message);
         void Fatal(string message);
         void Fatal(string message, Exception exception);
